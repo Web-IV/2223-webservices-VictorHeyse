71456ee69c95ee561f44f499addcd436ee55b55f
Victor Heyse <victor.heyse@student.hogent.be>
2022-12-07 18:04:38 +0100

    Fixed winston config + start implementing router

--- src/index.js
+++ src/index.js
@@ -1,36 +1,31 @@
 const Koa = require('koa');
 const config = require('config');
+const bodyParser = require('koa-bodyparser');
+const Router = require('@koa/router');
 const { getLogger, initializeLogger } = require('./core/logging')
-
-async function main() {
 
 // Initializing configs
 const NODE_ENV = config.get('env');
 const LOG_LEVEL = config.get('log.level');
 const LOG_DISABLED = config.get('log.disabled')
 
-
-// Initialize logger
-initializeLogger({
-    level: LOG_LEVEL,
-    disabled: LOG_DISABLED,
-    defaultMeta: {
-      NODE_ENV
-    },
-  });
-
 // Initializing Koa
 const app = new Koa();
 const logger = getLogger();
 
-app.use(async (ctx, next) => {
-    ctx.body = 'Hello World'
-    next();
-});
+// Initializing Router
+const router = new Router();
+
+// Defining routes
+router.get('/api/activies', async (ctx) => {
+
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.use(bodyParser());
 
 logger.info(`🚀 Server listening on http://localhost:9000`);
 app.listen(9000);
-
-}
-
-main()
---
+++ src/service/activity.js
@@ -0,0 +1,7 @@
+module.exports = {
+    getAll,
+	getById,
+	create,
+	updateById,
+	deleteById,
+}
