05aaa54b9c019286a23c557bc0ff6ec3b85b0a1a
Victor Heyse <victor.heyse@student.hogent.be>
2022-12-15 18:05:14 +0100

    test voor front end

--- src/service/activity.js
+++ src/service/activity.js
@@ -1,27 +1,44 @@
 const getAll = () => {
-    throw new Error("not implemented yet")
-}
+  const data = [
+    {
+      name: "Pretparkactiviteit",
+      date: "2021-07-01T12:32:04.534Z",
+      place: "lokaal klj",
+    },
+    {
+      name: "Testactiviteit",
+      date: "2022-09-02T12:32:04.534Z",
+      place: "hogent",
+    },
+    {
+      name: "Peter Heyse",
+      date: "2021-02-01T12:32:04.534Z",
+      place: "thuis",
+    },
+  ];
+  return data;
+};
 
 const getById = () => {
-    throw new Error("not implemented yet")
-}
+  throw new Error("not implemented yet");
+};
 
 const create = () => {
-    throw new Error("not implemented yet")
-}
+  throw new Error("not implemented yet");
+};
 
 const updateById = () => {
-    throw new Error("not implemented yet")
-}
+  throw new Error("not implemented yet");
+};
 
 const deleteById = () => {
-    throw new Error("not implemented yet")
-}
+  throw new Error("not implemented yet");
+};
 
 module.exports = {
-    getAll,
-	getById,
-	create,
-	updateById,
-	deleteById,
-}
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById,
+};
